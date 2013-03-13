05b62e673b851f1fede13d8bef6ea7ec9858c7b1
Marwijn <marwijn@hotmail.com>
2013-03-13 08:14:07 +0100

    Initial version

---
+++ AsyncWindsorProxy/AsyncInterceptor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Castle.Core;
+using Castle.Core.Interceptor;
+using Castle.DynamicProxy;
+
+namespace AsyncWindsorProxy
+{
+    public class Executer<TReturnType>
+    {
+        private readonly MethodInfo _targetMethod;
+        private readonly object _target;
+        private readonly object[] _args;
+
+        public Executer(MethodInfo targetMethod, object target, object[] args)
+        {
+            _targetMethod = targetMethod;
+            _target = target;
+            _args = args;
+        }
+
+        public TReturnType Execute()
+        {
+            return (TReturnType)_targetMethod.Invoke(_target, BindingFlags.InvokeMethod, null, _args, null);
+        }
+    }
+
+    public class AsyncInterceptor<T> : IInterceptor, IOnBehalfAware 
+    {
+        private readonly T _target;
+
+        public AsyncInterceptor(T target)
+        {
+            _target = target;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var methodName = invocation.Method.Name;
+            var targetMethod = typeof (T).GetMethod(methodName);
+            var returnType = targetMethod.ReturnType;
+            var functionType = typeof (Func<>).MakeGenericType(returnType);
+
+            var executertype = typeof (Executer<>).MakeGenericType(returnType);
+
+            var executeMethod = executertype.GetMethod("Execute");
+
+            var executer = Activator.CreateInstance(executertype, targetMethod, _target, invocation.Arguments);
+
+            dynamic function = Delegate.CreateDelegate(functionType, executer, executeMethod);
+
+            var factoryType = typeof (Task<>).MakeGenericType(returnType);
+            var factoryProperty = factoryType.GetProperty("Factory");
+            dynamic factory = factoryProperty.GetValue(null);
+
+            var task = factory.StartNew(function);
+
+            invocation.ReturnValue = task;
+        }
+
+        public void SetInterceptedComponentModel(ComponentModel target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AsyncWindsorProxyTests/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsyncWindsorProxy;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace AsyncWindsorProxyTests
+{
+    public class Caller
+    {
+        private readonly IAsync _sync;
+
+        public Caller(IAsync sync)
+        {
+            _sync = sync;
+        }
+
+        public int CallAsync(int x)
+        {
+            var task = _sync.Foo(x);
+            task.Wait();
+            return task.Result;
+        }
+}
+
+    public interface ISync
+    {
+        int Foo (int x);
+    }
+
+    public interface IAsync
+    {
+        Task<int> Foo(int x);
+    }
+
+    [TestFixture]
+    public class AsyncInterceptorTests
+    {
+        [Test]
+        public void InterceptorWillCallTarget()
+        {
+            var container = new WindsorContainer();
+
+            container.Register(
+                Component.For<Caller>(),
+           
+                Component.For<AsyncInterceptor<ISync>>(),
+                Component.For<IAsync>().Interceptors<AsyncInterceptor<ISync>>(),
+
+                Component.For<ISync>().UsingFactoryMethod(()=> MockRepository.GenerateStub<ISync>())
+                );
+
+            var caller = container.Resolve<Caller>();
+            var sync = container.Resolve<ISync>();
+
+            sync.Stub(s => s.Foo(5)).Return(6);
+
+            Assert.AreEqual(6, caller.CallAsync(5));
+        }
+    }
+}
